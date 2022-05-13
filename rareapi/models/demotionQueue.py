0298cf35b776170631eb1b03d6075ae0707cd17b
Matthew McMorries <matthew.mcmorries@gmail.com>
2022-05-13 13:55:24 -0500

    corrected fixtures, added basic post retrieve and list functionality

--- rareapi/models/demotionQueue.py
+++ rareapi/models/demotionQueue.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+
 class DemotionQueue(models.Model):
     action = models.CharField(max_length=20)
-    admin = models.ForeignKey("RareUser", on_delete=models.CASCADE)
-    approver_one = models.ForeignKey("RareUser", on_delete=models.CASCADE)
-    
+    admin = models.ForeignKey(
+        "RareUser", on_delete=models.CASCADE, related_name="+")
+    approver_one = models.ForeignKey(
+        "RareUser", on_delete=models.CASCADE, related_name="+")
